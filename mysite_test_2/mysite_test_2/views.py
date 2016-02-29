fd4b92ad74724c7071caae4cd555607c3a68153f
zjn1988 <774628544@qq.com>
2016-02-29 18:20:52 +0800

    DynamicsUrlFunction
    简单动态URL编程

--- mysite_test_2/mysite_test_2/views.py
+++ mysite_test_2/mysite_test_2/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-	
+from django.http import Http404
+import datetime
 def hello_from_mysite_test_2(request):
 	return HttpResponse("Hello world from mysite_test_2")
+
+	#动态URL	
+def dynamic_url_function(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
